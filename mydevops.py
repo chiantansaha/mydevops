d13a5d7cb47130393f1b148deb2b59c71a83c94e
chirantansaha <chirantansaha@gmail.com>
2019-08-03 12:31:47 +0800

    [MYJIRA-1235] Dev FIX - C Saha

--- mydevops.py
+++ mydevops.py
@@ -17,6 +17,8 @@
 import os, sys
 #import P4
 import string, shutil
+import random
+import math
 #import utils
 #import time
 #import ftplib
@@ -25,12 +27,27 @@ import string, shutil
 #import smtplib
 #import socket
 
-mylist = [ 'cheers', 'smile']
-performances = ['Ventriloquism', 'Amazing Acrobatics', 'Snake Charmer', 'Enchanted Elephants', 'Bearded Lady', 'Tiniest Man']
-mylist.append('cry')
-slang = {'cheerio':'goodbye', 'knackered':'tired', 'yonks':'ages'}
+#mylist = [ 'cheers', 'smile']
+#performances = ['Ventriloquism', 'Amazing Acrobatics', 'Snake Charmer', 'Enchanted Elephants', 'Bearded Lady', 'Tiniest Man']
+#mylist.append('cry')
+#slang = {'cheerio':'goodbye', 'knackered':'tired', 'yonks':'ages'}
 
 #mylist.remove('cheers')
 
-print(mylist,performances)
+#print(mylist,performances)
+total = 0 
+prices = [2.5,3.5,4.5]
 
+for price in prices:
+    print ('Price is now :', price)
+    total = total + price
+
+
+print(total)
+
+r1 = random.random()
+print(r1)
+
+for i in range(1,10):
+    ticket = random.randint(100, 1000)
+    print(i, 'Ticket no is : ',ticket)
