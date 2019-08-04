584d13649c84bb8a185e823534e9fbceb38a4ea4
chirantansaha <chirantansaha@gmail.com>
2019-08-04 10:20:05 +0800

    [JIRA1245] New day starts

--- mycode.py
+++ mycode.py
@@ -19,7 +19,7 @@ numbers = [4,5,65,98,3]
 avg = statistics.mean(numbers)
 print(avg)
 
-'''
+
 
 import random
 
@@ -34,3 +34,38 @@ def lotto_numbers():
 if __name__ == "__main__":
     i = lotto_numbers()
     print(i)
+'''
+
+import random
+def guessing_game():
+    num = random.randint(1, 10)
+
+    guess = int(input('Guess a number between 1 and 10 : '))
+    times = 1
+
+    while guess != num:
+        guess = int(input('Guess again'))
+        times += 1
+        if times == 3:
+            break
+
+    if guess == num:
+        print('You win!')
+    else:
+        print('You lose! The number was : ', num)
+    return guess
+
+def lotto_numbers():
+    lotto_nums = []
+    for i in range(5):
+        lotto_nums.append(random.randint(1, 53)) 
+    
+    return lotto_nums
+  
+def main():
+    numbers = lotto_numbers()
+    mynum = guessing_game()
+    print(numbers)
+    print(mynum)
+    
+main()
